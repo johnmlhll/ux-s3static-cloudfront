76774af58b4fb43086b0a51bfbace01c01f06506
johnmlhll <johnmlhll@yahoo.com>
2023-09-10 17:17:51 +0100

    1 - Drafted IaC and Frontend Code for Initial Deployment

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
+
+# This Project
+
+This project is focused on UX design and crafting old UX content (created by me in 2017) into a front end. It will also use AWS services to deploy a static website using Azure DevOps. Its UX focus makes it a content writing and web frontend project. It also has a DevOps project focus developing a static website in HTML, CSS, and Javascript (clientside). The CloudFormation template is customised for this project to firstly provision an s3 bucket, and then an AWS CLoudFront instance. Once done, the S3 bucket is bound via OAI policy to the CloudFront instance completing the templates automated provisioning of service infrastructure. The 2nd stage of the pipeline will deploy the app payload to the s3 bucket for rendering onto the cloudfront distribution. No custom domain was included in this project nor associated securing of its routing via TLS encryption over SSL. AWS's inbuilt securitizing of its DomainName will be used given this is a demo project for the promotion of good UX design by Maolte Technical Solutions.
+
 # ux-s3static-cloudfront
 Refined old project UX code and coded IaC for CloudFront S3 deployments to deploy under time pressure to the Sandbox AWS account.
+
+# Maolte Technical Solutions Limited
+
+This repo is the 4th in a series of demo deployments for the company I set up to pursue contracting. My intent for these repos is to demo my technological skills, mainly in infrastructure deployments over time. The idea is to show case reusable infrastructure as code (IaC) for cloud resources via Azure Devops and CloudFormation templating in AWS. I will, time permitting code up some fun apps for demo purposes and demo infrastructure as I go.
+
+Maolte Technical Solutions Limited was set up provide specialist services in the Cloud Infrastructre, DevOps and Writing. More at https://maolte.ie. Any queries, drop me a long at john@maolte.ie.
+
+Best Regards 
+John
+
+Founder | Engineer | Writer 
+Maolte Technical Solutions Limited
---
+++ code/scripts/javascript.js
@@ -0,0 +1,89 @@
+/**External Javascript File for Responsively Designed - Project Portfolio */
+/**
+ * Function 1 - openNav() Function to open the menu bar whilst in full screen or responsive mode 
+ * Function 2 - closeNav() Function to close the menu bar whilst in full screen mode or responsive mode
+ * Function 3 - slider() Listener function is a btn_expand class event listener for click events to expand project details 
+ * Function 4 - JQuery / JavaScript function to allow auto scroll back to top of view
+ * Function 5 - scrollUp() Scroll back to top of page when clicked
+ * Function 6 - swapImage(id,imageList) Swaps Image out from list supplied as an argument
+ * Function 7 -loadImages() Loads up images for slideshows on view
+ * Dev note: work around due to getElementById bug in ASP.NET core. seems to note like overloaded function
+ */
+function openNav() {
+    document.getElementById("nav_wrapper").style.opacity = "1";
+    document.getElementById("nav_wrapper").style.width = "22%";
+    document.getElementById("nav_wrapper").style.height = "auto";
+    document.getElementById("navbar_tab").style.display = "none";
+    document.getElementById("nav").style.display = "inline";
+    document.getElementById("nav").style.width = "90%";
+    document.getElementById("nav").style.height = "auto";
+    document.getElementById("unrestricted_nav").style.width = "88%";
+    document.getElementById("unrestricted_nav").style.height = "auto";
+ }
+
+function closeNav() {
+    document.getElementById("nav_wrapper").style.width = "6%";
+    document.getElementById("nav_wrapper").style.height = "13%";
+    document.getElementById("nav_wrapper").style.opacity = "0.5";
+    document.getElementById("nav").style.display = "none";
+    document.getElementById("navbar_tab").style.display = "inline";
+ }
+
+$(document).ready(function slider() {
+    $(".portfolio_entry_detail").hide();
+    $(".btn_expand").click(function() {
+        $(this).next("div").slideToggle(500); 
+    });
+});
+
+$(document).ready(function() {
+    $("#back_to_top").hide();
+    $(window).scroll(function() {
+        if($(window).scrollTop() >= 200){
+            $('#back_to_top').fadeIn();
+        } else {
+            $('#back_to_top').fadeOut();
+        }
+    });
+});
+
+function scrollUp() {
+    window.scrollTo(5,5);
+}
+
+function swapImage(id, imageList) {
+    var htmlSlideshowId = document.getElementById(id);
+    htmlSlideshowId.count = (htmlSlideshowId.count < 3) ? htmlSlideshowId.count : 0;
+    htmlSlideshowId.src = imageList[htmlSlideshowId.count];
+    htmlSlideshowId.count = (htmlSlideshowId.count + 1); imageList.length;
+}
+
+function loadImages() {
+    setInterval(function() {
+        swapImage('slideshow_csharp', [
+            "img/imgLogoCSharpVS.jpg",
+            "img/imgLogoMicrosoftSQLServer.png",
+            "img/imgLogoJava8.png"
+        ]);
+        swapImage('slideshow_bash', [
+            "img/imgLogoBash.png",
+            "img/imgLogoDotNet.png",
+            "img/imgLogoPostgreSQL.png"
+        ]);
+        swapImage('slideshow_xml', [
+            "img/imgLogoXML.png",
+            "img/imgLogoASPNETMVC.png",
+            "img/imgLogoHtml5Css3Js.png" 
+        ]);
+        swapImage('slideshow_android', [
+            "img/imgLogoAndroid.png",
+            "img/imgLogoAngular4TS.png",
+            "img/imgLogoMySQL.png"    
+        ]);
+        swapImage('slideshow_python', [
+            "img/imgLogoPython.png",
+            "img/imgLogoLinux.png",
+            "img/imgLogoFirebase.jpg"
+        ]);
+    }, 3000);
+}
